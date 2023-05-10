accd69cc2dd313e02151a490bcddcff36470e51a
mahesh1008 <solankimahesh1008@gmail.com>
2023-05-10 16:07:36 +0530

    intial-commit

---
+++ CreativeTournament/resources/views/teams/create.blade.php
@@ -0,0 +1,15 @@
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Team Name</label>
+    <input type="text" class="form-control" id="teamName" aria-describedby="emailHelp" placeholder="Enter Team Name">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Select Team Size</label>
+    <input type="select" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
